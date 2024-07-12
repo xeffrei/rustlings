4c8b2b3bc9487e51bb119a0cd30a6966f9d1fa32
Jeff Alexander <12126499+xeffrei@users.noreply.github.com>
2024-07-12 13:19:00 -0700

    Complete 03_if

--- exercises/03_if/if3.rs
+++ exercises/03_if/if3.rs
@@ -3,11 +3,11 @@ fn animal_habitat(animal: &str) -> &str {
     let identifier = if animal == "crab" {
         1
     } else if animal == "gopher" {
-        2.0
+        2
     } else if animal == "snake" {
         3
     } else {
-        "Unknown"
+        -1
     };
 
     // Don't change the expression below!
@@ -24,6 +24,7 @@ fn animal_habitat(animal: &str) -> &str {
 
 fn main() {
     // You can optionally experiment here.
+    println!("{}", animal_habitat("crab"));
 }
 
 // Don't change the tests!
--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,13 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(numberOfApples: i32) -> i32 {
+    if numberOfApples > 40 {
+        numberOfApples
+    } else {
+        numberOfApples * 2
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
