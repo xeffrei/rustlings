61ec8341f864029a7a742e981462a0a37c9e088c
Jeff Alexander <12126499+xeffrei@users.noreply.github.com>
2024-07-13 07:56:14 -0700

    Complete 05_vecs

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -3,7 +3,7 @@ fn array_and_vec() -> ([i32; 4], Vec<i32>) {
 
     // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
     // Use the vector macro.
-    // let v = ???;
+    let v = vec![a[0], a[1], a[2], a[3]];
 
     (a, v)
 }
